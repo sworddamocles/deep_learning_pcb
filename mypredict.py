32a26f00fd7d8cb1b7fb472ca039edf05202edc1
UltralyticsAssistant <web@ultralytics.com>
2026-04-20 12:54:28 +0000

    Auto-format by https://ultralytics.com/actions

--- mypredict.py
+++ mypredict.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 model = YOLO(r"yolo11n.pt")
 model.predict(
-#    source=r"ultralytics\assets",
+    #    source=r"ultralytics\assets",
     source=0,
     save=False,
     show=True,
